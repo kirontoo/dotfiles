1686327f4b99ff25927a07983895ac1c5a4513e7
Amy Dang <kirontoo@gmail.com>
2020-10-12 18:28:52 -0700

    update script to lock desktop

--- scripts/rofi-powermenu.sh
+++ scripts/rofi-powermenu.sh
@@ -4,12 +4,15 @@
 bindsym $mod+Escape exec gnome-screensaver-command --lock
 if [ -z "$@" ]; then
     echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
+    echo -en "Lockout\0icon\x1fsystem-lock\n"
     echo -en "Logout\0icon\x1fsystem-log-out\n"
     echo -en "Suspend\0icon\x1fsystem-suspend\n"
     echo -en "Reboot\0icon\x1fsystem-restart\n"
 else
     if [ "$1" = "Shutdown" ]; then
         shutdown now
+    elif [ "$1" = "Lockout" ]; then
+        gnome-session-command --lock
     elif [ "$1" = "Logout" ]; then
         gnome-session-quit
     elif [ "$1" = "Exit" ]; then
