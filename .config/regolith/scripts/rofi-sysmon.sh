fba7123ac5520d37dad67df2420f8bf84b600e8d
Amy Dang <kirontoo@gmail.com>
2020-10-17 00:41:50 -0700

    fix typos

--- .config/regolith/scripts/rofi-sysmon.sh
+++ .config/regolith/scripts/rofi-sysmon.sh
@@ -5,19 +5,18 @@ choice=$(echo "bashtop
 gtop
 gotop
 htop
-iftop" | rofi -dmenu -p "Syalacrittyem Monitor")
+iftop" | rofi -dmenu -p "System Monitor")
 	if [ $choice == 'htop' ]; then
-    echo $choice
-        exec alacritty -e htop
+        exec kitty -e htop
 	fi
 	if [ $choice == 'gtop' ]; then
-        exec alacritty -e gtop
+        exec kitty -e gtop
 	fi
 	if [ $choice == 'gotop' ]; then
-        exec alacritty -e gotop 
+        exec kitty -e gotop 
 	fi
-	if [ $choice == 'ifton' ]; then
-        exec alacritty -e ifton 
+	if [ $choice == 'iftop' ]; then
+        exec kitty -e iftop
 	fi
-	if [ $choices == 'bashtop' ]; then
-				exec alacritty -e bashtop
+	if [ $choice == 'bashtop' ]; then
+				exec kitty -e bashtop
