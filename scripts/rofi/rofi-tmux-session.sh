dbb8a593968d9b46224accb19bc1fa28c45d0e6e
Amy Dang <kirontoo@gmail.com>
2020-11-28 18:30:58 -0800

    change rofi theme used

--- scripts/rofi/rofi-tmux-session.sh
+++ scripts/rofi/rofi-tmux-session.sh
@@ -10,7 +10,7 @@ attached=$( tmux display -p '#{session_name}' )
 TMUX_SESSION=$( (echo 'new'; tmux_sessions) | rofi -dmenu -p "Select tmux session" -width 15 -lines $( expr $( tmux list-session -F '#S' | wc -l ) + 1))
 
 if [[ x"new" = x"${TMUX_SESSION}" ]]; then
-		session_name=$( rofi -dmenu -p "Session Name?" -width 30 )
+		session_name=$( rofi -dmenu -p "Session Name?" -theme '.config/rofi/prompt.rasi' )
 		[[ -z "${session_name}" ]] && exit
 		rofi-sensible-terminal -e tmux new -t $session_name &
 elif [[ -z "${TMUX_SESSION}" ]]; then
