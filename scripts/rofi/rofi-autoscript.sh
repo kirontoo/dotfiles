f9e9532415a3c8eabc8c44053697099cfda2920b
Amy Dang <kirontoo@gmail.com>
2020-11-29 00:29:15 -0800

    open script in a floating terminal

--- scripts/rofi/rofi-autoscript.sh
+++ scripts/rofi/rofi-autoscript.sh
@@ -2,4 +2,4 @@
 
 selected=$(ls "$HOME/scripts" | rofi -dmenu -width 15 -lines 10 -p "autoscript")
 [[ -z $selected ]] && exit
-$HOME/scripts/autoscript $selected rofi &
+$HOME/scripts/autoscript $selected true &
