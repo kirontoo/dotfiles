eeab8b91f201185f1e269e7ac83fd15929db08f0
Amy Dang <kirontoo@gmail.com>
2020-12-02 02:21:31 -0800

    fix: wrong error message

--- scripts/rofi/rofi-sysmon.sh
+++ scripts/rofi/rofi-sysmon.sh
@@ -7,29 +7,16 @@ htop
 iftop" | rofi -dmenu -p "System Monitor" -width 15 -lines 5)
 if [ "$choice" == 'htop' ]; then
 	kitty --name htop -e htop
-fi
 if [ "$choice" == 'gtop' ]; then
 	kitty --name gtop -e gtop
-else
-	rofi -theme "$HOME/.config/rofi/prompt.rasi" -e "Please install 'gtop' first."
-fi
-if [ "$choice" == 'gotop' ]; then
+elif [ "$choice" == 'gotop' ]; then
 	kitty --name gotop -e gotop 
-else
-	rofi -theme "$HOME/.config/rofi/prompt.rasi" -e "Please install 'gotop' first."
-fi
-if [ "$choice" == 'iftop' ]; then
+elif [ "$choice" == 'iftop' ]; then
 	kitty --name iftop -e sudo iftop
-else
-	rofi -theme "$HOME/.config/rofi/prompt.rasi" -e "Please install 'iftop' first."
-fi
-if [ "$choice" == 'bashtop' ]; then
+elif [ "$choice" == 'bashtop' ]; then
 	kitty --name bashtop -e bashtop
-else
-	rofi -theme "$HOME/.config/rofi/prompt.rasi" -e "Please install 'bashtop' first."
-fi
-if [ "$choice" == 'bandwhich' ]; then
+elif [ "$choice" == 'bandwhich' ]; then
 	kitty --name bandwhich -e sudo bandwhich
 else
-	rofi -theme "$HOME/.config/rofi/prompt.rasi" -e "Please install 'bandwhich' first."
+	rofi -theme "$HOME/.config/rofi/prompt.rasi" -e "Please install ${choice} first."
 fi
