5eb4c837c94fe1e412d8a49944a066d40a529ffa
Amy Dang <kirontoo@gmail.com>
2020-12-06 15:41:06 -0800

    focus window if session is already open

--- scripts/rofi/rofi-tmux-session.sh
+++ scripts/rofi/rofi-tmux-session.sh
@@ -10,13 +10,16 @@ attached=$( tmux display -p '#{session_name}' )
 TMUX_SESSION=$( (echo 'new'; tmux_sessions) | rofi -dmenu -p "Select tmux session" -width 15 -lines $( expr $( tmux list-session -F '#S' | wc -l ) + 1))
 
 if [[ x"new" = x"${TMUX_SESSION}" ]]; then
+		# create a new tmux session
 		session_name=$( rofi -dmenu -p "Session Name?" -theme '.config/rofi/prompt.rasi' )
 		[[ -z "${session_name}" ]] && exit
 		i3-sensible-terminal --name tmux_${session_name} --title ${session_name} -e tmux new -t $session_name &
+
 elif [[ -z "${TMUX_SESSION}" ]]; then
 		exit
 else
-    i3-sensible-terminal --name tmux_${TMUX_SESSION} --title ${TMUX_SESSION} -e tmux attach -t "${TMUX_SESSION}" &
+		# attach to a session
+    i3run -i tmux_${TMUX_SESSION} -e "i3-sensible-terminal --name tmux_${TMUX_SESSION} --title ${TMUX_SESSION} -e tmux attach -t '${TMUX_SESSION}'" &
 fi
 
 exit
