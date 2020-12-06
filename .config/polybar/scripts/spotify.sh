f3ca44da2fc2bd76b9cf5c180f9c8836e3fa86df
Amy Dang <kirontoo@gmail.com>
2020-12-06 13:39:10 -0800

    add support for spotifyd

--- .config/polybar/scripts/spotify.sh
+++ .config/polybar/scripts/spotify.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 main() {
-  if ! pgrep -x spotify >/dev/null; then
-    echo ""; exit
+	spotify_source=""
+  if pgrep -x spotifyd >/dev/null; then
+		spotify_source="spotifyd"
+	elif pgrep -x spotifyd >/dev/null; then
+		spotify_source="spotify"
+	else 
+		echo ""; exit
   fi
+
 
   cmd="org.freedesktop.DBus.Properties.Get"
   domain="org.mpris.MediaPlayer2"
   path="/org/mpris/MediaPlayer2"
 
-  meta=$(dbus-send --print-reply --dest=${domain}.spotify \
+  meta=$(dbus-send --print-reply --dest=${domain}.spotifyd \
     /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
 
   artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1  | sed 's/\&/\\&/g' | sed 's#\/#\\/#g')
