494968cc3eb7fa2cc4bcf74302f0329600db2563
Amy Dang <kirontoo@gmail.com>
2020-12-07 16:33:48 -0800

    bug: checks for wrong spotify process

--- .config/polybar/scripts/spotify.sh
+++ .config/polybar/scripts/spotify.sh
@@ -4,7 +4,7 @@ main() {
 	spotify_source=""
   if pgrep -x spotifyd >/dev/null; then
 		spotify_source="spotifyd"
-	elif pgrep -x spotifyd >/dev/null; then
+	elif pgrep -x spotify >/dev/null; then
 		spotify_source="spotify"
 	else 
 		echo ""; exit
@@ -15,7 +15,7 @@ main() {
   domain="org.mpris.MediaPlayer2"
   path="/org/mpris/MediaPlayer2"
 
-  meta=$(dbus-send --print-reply --dest=${domain}.spotifyd \
+  meta=$(dbus-send --print-reply --dest=${domain}.${spotify_source} \
     /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
 
   artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1  | sed 's/\&/\\&/g' | sed 's#\/#\\/#g')
