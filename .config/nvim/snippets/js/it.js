8f910ecaacad45b74f4526825c9240ea970ccdc4
Amy Dang <kirontoo@gmail.com>
2020-12-08 10:00:12 -0800

    update snippet

--- .config/nvim/snippets/js/it.js
+++ .config/nvim/snippets/js/it.js
@@ -1,6 +1,8 @@
-it( '', async () => {
+it( '', async ( done ) => {
 	const response = await supertest( app )
 		.get( '/api/v1/route' )
 		.expect( 'Content-Type', /json/ )
 		.expect( 200 );
+
+	done();
 });
