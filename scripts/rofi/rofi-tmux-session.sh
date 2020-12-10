770d9ed2e2f840cead2502017907e1f755b528cb
Amy Dang <kirontoo@gmail.com>
2020-12-09 16:56:50 -0800

    feat: Option to kill sessions

--- scripts/rofi/rofi-tmux-session.sh
+++ scripts/rofi/rofi-tmux-session.sh
@@ -7,13 +7,17 @@ function tmux_sessions()
 }
 
 attached=$( tmux display -p '#{session_name}' )
-TMUX_SESSION=$( (echo 'new'; tmux_sessions) | rofi -dmenu -p "Select tmux session" -width 15 -lines $( expr $( tmux list-session -F '#S' | wc -l ) + 1))
+TMUX_SESSION=$( (echo 'new'; echo 'kill'; tmux_sessions) | rofi -dmenu -p "Select tmux session" -width 15 -lines $( expr $( tmux list-session -F '#S' | wc -l ) + 1))
 
 if [[ x"new" = x"${TMUX_SESSION}" ]]; then
 		# create a new tmux session
 		session_name=$( rofi -dmenu -p "Session Name?" )
 		[[ -z "${session_name}" ]] && exit
 		i3run -i tmux_${TMUX_SESSION} -e "i3-sensible-terminal --name tmux_${session_name} --title ${session_name} -e tmux new -t '${session_name}'" &
+
+elif [[ x"kill" = x"${TMUX_SESSION}" ]]; then
+		TMUX_SESSION=$( ( tmux_sessions) | rofi -dmenu -p "Select tmux session" -width 15 -lines $( expr $( tmux list-session -F '#S' | wc -l ) + 1))
+		tmux kill-session -t ${TMUX_SESSION} 
 
 elif [[ -z "${TMUX_SESSION}" ]]; then
 		exit
