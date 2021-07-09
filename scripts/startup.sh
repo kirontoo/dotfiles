c909a58e12d60e009ed0342b4c30d67d3658d2b2
Amy Dang <kirontoo@gmail.com>
2021-07-08 20:30:40 -0700

    Update startup script to throw away errors

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec i3-sensible-terminal --detach --name schedule -e nvim $HOME/vimwiki/daily-goals/index.md
+i3-sensible-terminal --detach --name schedule -e nvim -c VimwikiDiaryIndex
+exec $HOME/scripts/tmux-dev > /dev/null 2>&1
