fec9fa88680c8514ffd9fbb1a40509da784e5840
Amy Dang <kirontoo@gmail.com>
2022-06-15 19:38:26 -0700

    Fix terminal not launching

--- scripts/rofi/rofi-tmux-session.sh
+++ scripts/rofi/rofi-tmux-session.sh
@@ -13,7 +13,7 @@ if [[ x"new" = x"${TMUX_SESSION}" ]]; then
     # create a new tmux session
     session_name=$( rofi -dmenu -p "Session Name?" )
     [[ -z "${session_name}" ]] && exit
-    i3run -i tmux_${session_name} -e "i3-sensible-terminal --name tmux_${session_name} --title ${session_name} -e tmux new -s '${session_name}'" &
+    i3run -i tmux_${session_name} -e "i3-sensible-terminal --name tmux_${session_name} --title ${session_name} -e tmux new -s ${session_name}" &
 
 elif [[ x"kill" = x"${TMUX_SESSION}" ]]; then
     TMUX_SESSION=$( ( tmux_sessions) | rofi -dmenu -p "Select tmux session" -width 15 -lines $( expr $( tmux list-session -F '#S' | wc -l ) + 1))
@@ -23,7 +23,7 @@ elif [[ -z "${TMUX_SESSION}" ]]; then
     exit
 else
     # attach to a session
-    i3run -i tmux_${TMUX_SESSION} -e "i3-sensible-terminal --name tmux_${TMUX_SESSION} --title ${TMUX_SESSION} -e tmux attach -t '${TMUX_SESSION}'" &
+    i3run -i tmux_${TMUX_SESSION} -e "i3-sensible-terminal --name tmux_${TMUX_SESSION} --title ${TMUX_SESSION} -e tmux attach -t ${TMUX_SESSION}" &
 fi
 
 exit
