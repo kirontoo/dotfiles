b3834d8c764044661e7579e8f2cfe4d4e7d996ca
Amy Dang <kirontoo@gmail.com>
2023-10-09 11:00:31 -0700

    update scripts

--- scripts/multi-monitors.sh
+++ scripts/multi-monitors.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-xrandr --output DP-2 --primary --auto --output DP-2 --auto --right-of HDMI-0
+# xrandr --auto --output DP-2 --primary --auto --output DP-2 --auto --right-of HDMI-0 --scale 0.8
+xrandr --auto --output DP-2 --primary --right-of HDMI-0 --scale 1x1
+
+
+# xrandr \
+#     --fb 7680x2160\
+#     --output HDMI-0 --scale 1.5x1.5\
+#     --output DP-2 --primary --scale 1x1 --pos 3840x0
